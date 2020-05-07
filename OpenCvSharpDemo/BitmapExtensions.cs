e8c13049e6f7fd522cd1932fcc8d18eb1252eb66
sekulicb <sekulicb1@gmail.com>
2020-05-07 18:07:16 +0200

    Initial commit

---
+++ OpenCvSharpDemo/BitmapExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+
+namespace OpenCvSharpDemo
+{
+    public static class BitmapExtensions
+    {
+        public static ImageSource ConvertToWpfImage(this Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException(nameof(bitmap));
+
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
+
+        public static Bitmap ConvertoToBitmap(this BitmapSource srs)
+        {
+            var width = srs.PixelWidth;
+            var height = srs.PixelHeight;
+            var stride = width * ((srs.Format.BitsPerPixel + 7) / 8);
+
+            var ptr = IntPtr.Zero;
+            try
+            {
+                ptr = Marshal.AllocHGlobal(height * stride);
+
+                srs.CopyPixels(new Int32Rect(0, 0, width, height), ptr, height * stride, stride);
+
+                using (var btm = new Bitmap(width, height, stride, PixelFormat.Format1bppIndexed, ptr))
+                {
+                    // Clone the bitmap so that we can dispose it and
+                    // release the unmanaged memory at ptr
+                    return new Bitmap(btm);
+                }
+            }
+            finally
+            {
+                if (ptr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(ptr);
+                }
+            }
+        }
+    }
+}
---
+++ OpenCvSharpDemo/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Window = System.Windows.Window;
+
+namespace OpenCvSharpDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VideoCapture cap;
+        WriteableBitmap wb;
+        const int frameWidth = 1600;
+        const int frameHeight = 1200;
+        bool loop = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (InitWebCamera())
+            {
+                MessageBox.Show("Webcam is on.");
+            }
+            else
+            {
+                MessageBox.Show("There is a problem with the webcam.");
+            }
+        }
+
+        private bool InitWebCamera()
+        {
+            try
+            {
+                cap = VideoCapture.FromCamera(0, VideoCaptureAPIs.DSHOW);
+                cap.Open(0);
+                cap.FrameWidth = frameWidth;
+                cap.FrameHeight = frameHeight;
+
+                wb = new WriteableBitmap(cap.FrameWidth, cap.FrameHeight, 96, 96, PixelFormats.Bgr24, null);
+                image.Source = wb;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            loop = false;
+            if (cap.IsOpened())
+            {
+                cap.Dispose();
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Mat frame = new Mat();
+            Cv2.NamedWindow("1", WindowMode.AutoSize);
+            loop = true;
+            while (loop)
+            {
+
+
+                if (cap.Read(frame))
+                {
+                    Cv2.ImShow("1", frame);
+                    var t = frame.ToBitmap();
+                    var bitmap = t.ConvertToWpfImage();
+                    image.Source = bitmap;
+                }
+
+                int c = Cv2.WaitKey(10); 
+                if (c  != -1 )
+                    break;
+
+            }
+        }
+
+        private void Button2_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (var fileStream = new FileStream(string.Format($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{Guid.NewGuid()}.png"), FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+               
+                encoder.Frames.Add(BitmapFrame.Create((BitmapSource)image.Source));
+                encoder.Save(fileStream);
+            }
+        }
+    }
+}
